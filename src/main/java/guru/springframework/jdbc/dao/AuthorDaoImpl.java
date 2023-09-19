f19fe82797d13a303f36544b874f4ba0270f7d9b
nyguerrillagirl <lorraine.figueroa@brainycode.com>
2023-09-19 19:56:45 -0400

    Added RowMapper class for Author.

--- src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -1,6 +1,7 @@
 package guru.springframework.jdbc.dao;
 
 import guru.springframework.jdbc.domain.Author;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -28,5 +29,9 @@ public class AuthorDaoImpl implements AuthorDao {
     @Override
     public void deleteAuthorById(Long id) {
 
+    }
+
+    private RowMapper<Author> getRowMapper() {
+        return new AuthorMapper();
     }
 }
